c47e808067ea14209d76b121e394dfbaf5b2b483
Jorge L <kolmax@gmail.com>
2018-06-24 16:20:11 +0200

    Start the logic layer

---
+++ js/pipe.js
@@ -0,0 +1,21 @@
+function Pipe() {
+  this.x
+  this.y
+  this.type
+  this.orientation
+}
+
+//Called when
+Pipe.prototype.rotateLeft = function() {
+
+}
+
+Pipe.prototype.rotateRight = function() {
+
+}
+
+
+//Called when beer reaches the pipe and it cant be moved
+Pipr.prototype.block = function() {
+
+}
